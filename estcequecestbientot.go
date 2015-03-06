df15dc8db3f8eeabb0c84e3a64cbe94f0f32c9e3
ArnAud Cordier <arnaud@mutins.net>
2015-03-06 17:36:03 +0100

    Fichiers json

--- estcequecestbientot.go
+++ estcequecestbientot.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	app := estcequecest.NewApp("messages", "messages_")
 	app.Load("object")
+	app.Load("noon")
+	app.Load("apero")
+	app.Load("pause")
 	fmt.Printf("%s", app)
 	messages := app.GetMessages()
 	fmt.Printf("Messages : %v\n", messages)
