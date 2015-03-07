e47b7fcfcaf0e3f7d040bd50e89496d5f9e34981
ArnAud Cordier <arnaud@mutins.net>
2015-03-07 16:25:03 +0100

    One main() is enough

--- estcequecestbientot.go
+++ estcequecestbientot.go
@@ -1,22 +1,61 @@
+// estcequecestbientot HTTP server
+// usage:
+// PORT=8080 HOST=localhost ./estcequecestbientot
 package main
 
 import (
 	"estcequecestbientot/estcequecest"
 	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
 )
 
-func main() {
-	app := estcequecest.NewApp("messages", "messages_")
-	// 	app.Load("object")
-	app.Load("noon")
-	app.Load("apero")
-	app.Load("pause")
-	list, list2 := app.List()
-	fmt.Printf("%v %v\n", list, list2)
-	app.Unload("apero")
-	list, list2 = app.List()
-	fmt.Printf("%v %v\n", list, list2)
+var app *estcequecest.App
 
+func main() {
+	app = estcequecest.NewApp("messages", "messages_")
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/load/", load)
+	http.HandleFunc("/unload/", unload)
+	http.HandleFunc("/list", list)
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	host := os.Getenv("HOST")
+	fmt.Println("Est-ce-que c'est bientôt? running on " + host + ":" + port)
+
+	http.ListenAndServe(host+":"+port, nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
 	messages := app.GetMessages()
-	fmt.Printf("Messages : %v\n", messages)
+	t, _ := template.ParseFiles("templates/estcequecestbientot.html")
+	t.Execute(w, &messages)
+}
+
+func load(w http.ResponseWriter, r *http.Request) {
+	name := strings.Replace(r.URL.Path, "/load/", "", 1)
+	if len(name) > 0 {
+		app.Load(name)
+	}
+	http.Redirect(w, r, "/", 303)
+}
+
+func unload(w http.ResponseWriter, r *http.Request) {
+	name := strings.Replace(r.URL.Path, "/unload/", "", 1)
+	if len(name) > 0 {
+		app.Unload(name)
+	}
+	http.Redirect(w, r, "/", 303)
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	notloaded, loaded := app.List()
+	t, _ := template.ParseFiles("templates/list.html")
+	t.Execute(w, map[string]*[]string{"notloaded": &notloaded, "loaded": &loaded})
 }
