2411723198851a26df0234655c2f9b1a1476e2f4
ArnAud Cordier <arnaud@mutins.net>
2015-03-07 16:12:26 +0100

    HTTP: serve all routes

--- estcequecestbientotHTTP.go
+++ estcequecestbientotHTTP.go
@@ -7,28 +7,53 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"strings"
 )
 
 var app *estcequecest.App
 
 func main() {
 	app = estcequecest.NewApp("messages", "messages_")
-	app.Load("pause")
-	app.Load("apero")
 
 	http.HandleFunc("/", index)
+	http.HandleFunc("/load/", load)
+	http.HandleFunc("/unload/", unload)
+	http.HandleFunc("/list", list)
 
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
 	}
 	host := os.Getenv("HOST")
-	fmt.Println("App running on " + host + ":" + port)
+	fmt.Println("Est-ce-que c'est running on " + host + ":" + port)
+
 	http.ListenAndServe(host+":"+port, nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	messages := app.GetMessages()
 	t, _ := template.ParseFiles("templates/estcequecestbientot.html")
-	t.Execute(w, messages)
+	t.Execute(w, &messages)
+}
+
+func load(w http.ResponseWriter, r *http.Request) {
+	name := strings.Replace(r.URL.Path, "/load/", "", 1)
+	if len(name) > 0 {
+		app.Load(name)
+	}
+	http.Redirect(w, r, "/", 303)
+}
+
+func unload(w http.ResponseWriter, r *http.Request) {
+	name := strings.Replace(r.URL.Path, "/unload/", "", 1)
+	if len(name) > 0 {
+		app.Unload(name)
+	}
+	http.Redirect(w, r, "/", 303)
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	notloaded, loaded := app.List()
+	t, _ := template.ParseFiles("templates/list.html")
+	t.Execute(w, map[string]*[]string{"notloaded": &notloaded, "loaded": &loaded})
 }
